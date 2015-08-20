73fa215a69c6bba3cd01f7b969815de035b31087
Damien Clauzel <Damien@Clauzel.eu>
2015-08-20 18:35:16 +0000

    + StatsSurSoumissions.py mise à jour du LisezMoi fix mineur InformationsSurSoumissions.py nettoyage

--- StatsSurSoumissions.py
+++ StatsSurSoumissions.py
@@ -9,6 +9,11 @@
 
 import praw
 import OAuth2Util
+import datetime
+import collections
+from collections import OrderedDict
+from collections import Counter
+
 
 ###########
 # Connexion
@@ -35,7 +40,7 @@ sousjlailu = r.get_subreddit("Europe")
 
 
 
-##################################
+###########################################################################################
 # On recherche dans les soumissions récentes celles dont le titre contient un des mot-clefs
 
 
@@ -54,23 +59,37 @@ print("Marqueurs: ", end="")
 print(", ".join(marqueurs))
 print("_____")
 
+# liste des auteurs suspects
+auteurs = []
+
+# liste par heure des soumissions suspectes
+horaires = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
 # pour chaque soumission
-for soumission in sousjlailu.get_new(limit=1000):
+for soumission in sousjlailu.get_new(limit=100):
 	try:
 		titre = soumission.title
 		auteur = soumission.author.name
-		permalien = soumission.permalink
-
+		heureCréation = int(datetime.datetime.fromtimestamp(soumission.created_utc).strftime('%k'))
+	
 		# si le mot-clef est dans le titre
 		if any(mot.lower() in titre.lower() for mot in marqueurs):
-			print("{0}\n/u/{1}\n{2}\n_____".format(
-				titre,
-				auteur,
-				permalien,
-				) )
-
+			auteurs.append(auteur)
+			horaires[heureCréation]+=1
+	
 	except AttributeError as e:
 		# les soumissions supprimées font exploser la moulinette
 		pass
+
+####################
+# calculs des scores
+
+print("Horaires : {0}".format(horaires))
+
+# De la liste des auteurs on crée une collection pour en faire un dictionnaire trié par ordre décroissant sur le nombre de soumissions
+auteursComptés = OrderedDict( sorted(collections.Counter(auteurs).items(), key=lambda t: t[1], reverse=True) ) 
+
+for auteur in auteursComptés.items():
+	print( "{0} : {1}".format(auteur[0], auteur[1]) )
 
 # vim: tabstop=3 shiftwidth=3 noexpandtab
